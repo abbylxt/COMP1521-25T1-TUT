8e42cb6b9092debf2ed76950873ada40f12f54cf
abbylxt <z5362344@unsw.edu.au>
2025-03-18 12:30:51 +1100

    w5 init

---
+++ W4/q5.s
@@ -0,0 +1,76 @@
+main:
+main_prologue:
+	begin
+	push	$ra
+
+main_body:
+	# sum4(11, 13, 17, 19);
+	li	$a0, 11
+	li	$a1, 13
+	li	$a2, 17
+	li	$a3, 19
+
+	jal	sum4
+
+	move	$a0, $v0
+	li	$v0, 1
+	syscall
+
+	li	$a0, '\n'
+	li	$v0, 11
+	syscall
+
+main_epilogue:
+
+	pop	$ra
+	li	$v0, 0
+	jr	$ra
+
+sum4:
+	# li	$a0, a
+	# li	$a1, b
+	# li	$a2, c
+	# li	$a3, d
+sum4_prologue:
+	begin 	
+	push	$ra
+	push	$s0
+	push	$s1
+	push	$s2
+	push	$s3
+sum4_body:
+	move	$s2, $a2
+	move	$s3, $a3
+	# already $a0 = a, $a1 = b
+	jal	sum2
+	move	$s0, $v0
+
+	move	$a0, $s2
+	move	$a1, $s3
+	jal	sum2
+
+	move	$s1, $v0
+	
+	move	$a0, $s0
+	move	$a1, $s1
+	jal	sum2
+
+sum4_epilogue:
+	pop	$s3
+	pop	$s2
+	pop	$s1
+	pop	$s0
+	pop	$ra
+	end
+
+	jr	$ra
+
+sum2:
+sum2_prologue:
+
+sum2_body:
+	add	$v0, $a0, $a1
+	
+sum2_epilogue:
+	jr	$ra
+
